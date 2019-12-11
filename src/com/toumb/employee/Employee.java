ad6f1b627e672fce196386de091eaecea1ea91b9
Giannis Toumboglou <ioannis.toumboglou@gmail.com>
2019-12-11 21:59:23 +0200

    Employee and DBUtil classes

--- src/com/toumb/employee/Employee.java
+++ src/com/toumb/employee/Employee.java
@@ -1,8 +1,139 @@
 package com.toumb.employee;
 
+import java.sql.Date;
+
 public class Employee {
 	private int id;
+	private String title;
 	private String firstName;
+	private String middleName;
 	private String lastName;
+	private String jobTitle;
+	private String email;
+	private String phone;
+	private Date dateOfBirth;
+	private String address;
+	
+	public Employee(int id, String title, String firstName, String middleName, String lastName, String jobTitle,
+			String email, String phone, Date dateOfBirth, String address) {
+		this.id = id;
+		this.title = title;
+		this.firstName = firstName;
+		this.middleName = middleName;
+		this.lastName = lastName;
+		this.jobTitle = jobTitle;
+		this.email = email;
+		this.phone = phone;
+		this.dateOfBirth = dateOfBirth;
+		this.address = address;
+	}
+	
+	public Employee(int id, String title, String firstName, String lastName, String email) {
+		this.id = id;
+		this.title = title;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+	
+	public Employee(int id, String firstName, String lastName, String email) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+	
+	public Employee(String firstName, String lastName, String email) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getMiddleName() {
+		return middleName;
+	}
+
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getJobTitle() {
+		return jobTitle;
+	}
+
+	public void setJobTitle(String jobTitle) {
+		this.jobTitle = jobTitle;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", title=" + title + ", firstName=" + firstName + ", middleName=" + middleName
+				+ ", lastName=" + lastName + ", jobTitle=" + jobTitle + ", email=" + email + ", phone=" + phone
+				+ ", dateOfBirth=" + dateOfBirth + ", address=" + address + "]";
+	}
+	
 }
---
+++ src/com/toumb/employee/EmployeeDBUtil.java
@@ -0,0 +1,172 @@
+package com.toumb.employee;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.List;
+import java.util.ArrayList;
+
+public class EmployeeDBUtil {
+	private DataSource theDataSource;
+	
+	// Method to retrieve Employees from database
+	public List<Employee> getEmployees() throws Exception {
+		List<Employee> employees = new ArrayList<>();
+		
+		Connection myConn = null;
+		Statement myStatement = null;
+		ResultSet myRes = null;
+		
+		try {
+			// Get a connection
+			myConn = theDataSource.getConnection();
+			// Create an SQL statement
+			String sql = "SELECT * FROM employee ORDER BY last_name";
+			myStatement = myConn.createStatement();
+			// Execute the query
+			myRes = myStatement.executeQuery(sql);
+			// Process the result
+			while(myRes.next()) {
+				// Retrieve data from row
+				int id = myRes.getInt("id");
+				String firstName = myRes.getString("first_name");
+				String lastName = myRes.getString("last_name");
+				String email = myRes.getString("email");
+				//Create a new Employee and add to the employee list
+				Employee tempEmployee = new Employee(id, firstName, lastName, email);
+				employees.add(tempEmployee);
+			}
+			return employees;
+			
+		} finally {
+			// Close all the JDBC objects
+			close(myConn, myStatement, myRes);
+		}
+	}
+
+	// Method to close JDBC objects
+	private void close(Connection myConn, Statement myStatement, ResultSet myRes) {
+		try {
+			if(myRes != null)
+				myRes.close();
+			if(myStatement != null)
+				myStatement.close();
+			if(myConn != null)
+				myConn.close();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// Method to insert an Employee in the database
+	public void addEmployee(Employee theEmployee) throws Exception {
+		Connection myConn = null;
+		PreparedStatement myStatement = null;
+		
+		try {
+			// Connect to database
+			myConn = theDataSource.getConnection();
+			// Create SQL query for insert
+			String sql = "INSERT INTO employee (first_name, last_name, email) VALUES (?, ?, ?)";
+			myStatement = myConn.prepareStatement(sql);
+			myStatement.setString(1, theEmployee.getFirstName());
+			myStatement.setString(2, theEmployee.getLastName());
+			myStatement.setString(3, theEmployee.getEmail());
+			// Execute SQL query
+			myStatement.execute();
+		} finally {
+			// Close all the JDBC objects
+			close(myConn, myStatement, null);
+		}
+	}
+	
+	// Method to retrieve an Employee using the id
+	public Employee getEmployee(String theEmployeeId) throws Exception {
+		Employee employee = null;
+		Connection myConn = null;
+		PreparedStatement myStatement = null;
+		ResultSet myRes = null;
+		int employeeId;
+		
+		try {
+			// Convert employee id to int
+			employeeId = Integer.parseInt(theEmployeeId);
+			// Connect to database
+			myConn = theDataSource.getConnection();
+			// Create an SQL statement
+			String sql = "SELECT * FROM employee WHERE id=?";
+			// Create prepared statement
+			myStatement = myConn.prepareStatement(sql);
+			// Set parameters
+			myStatement.setInt(1, employeeId);
+			// Execute statement
+			myRes = myStatement.executeQuery();
+			// Retrieve data from database row
+			if(myRes.next()) {
+				String firstName = myRes.getString("first_name");
+				String lastName = myRes.getString("last_name");
+				String email = myRes.getString("email");
+				// Create an Employee
+				employee = new Employee(employeeId, firstName, lastName, email);
+			} else {
+				throw new Exception("Unable to find Employee Id: " + employeeId);
+			}
+			return employee;
+			
+		} finally {
+			// Close all the JDBC objects
+			close(myConn, myStatement, myRes);
+		}	
+	}
+	
+	// Method to update an Employee record
+	public void updateEmployee(Employee employee) throws Exception {
+		Connection myConn = null;
+		PreparedStatement myStatement = null;
+		
+		try {
+			// Connect to database
+			myConn = theDataSource.getConnection();
+			// Create an SQL statement
+			String sql = "UPDATE employee SET first_name=?, last_name=?, email=? WHERE id=?";
+			// Create prepared statement
+			myStatement = myConn.prepareStatement(sql);
+			// Set parameters
+			myStatement.setString(1, employee.getFirstName());
+			myStatement.setString(2, employee.getLastName());
+			myStatement.setString(3, employee.getEmail());
+			myStatement.setInt(4, employee.getId());
+			// Execute SQL query
+			myStatement.execute();
+		} finally {
+			// Close all the JDBC objects
+			close(myConn, myStatement, null);
+		}
+	}
+	
+	// Method to delete an Employee using the id
+	public void deleteEmployee(String theEmployeeId) throws Exception {
+		Connection myConn = null;
+		PreparedStatement myStatement = null;
+		
+		try {
+			// Convert employee id to integer
+			int employeeId = Integer.parseInt(theEmployeeId);
+			// Connect to database
+			myConn = theDataSource.getConnection();
+			// Create an SQL statement
+			String sql = "DELETE FROM employee WHERE id=?";
+			// Create prepared statement
+			myStatement = myConn.prepareStatement(sql);
+			// Set parameters
+			myStatement.setInt(1, employeeId);
+			// Execute statement
+			myStatement.execute();
+		} finally {
+			// Close all the JDBC objects
+			close(myConn, myStatement, null);
+		}
+	}
+}
